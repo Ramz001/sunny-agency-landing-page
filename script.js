4804c8a4aaa9e221438aff1ebe0ce469438d6def
Ramz001 <rkenjaev1@gmail.com>
2021-12-29 18:30:36 +0500

    main

--- script.js
+++ script.js
@@ -2,10 +2,5 @@ const threeBars = document.querySelector(".three-bars")
 const navLinks = document.querySelector('.nav-links')
 
 threeBars.addEventListener("click", () => {
-    if(navLinks.classList.contains("show-links")){
-        navLinks.classList.remove("show-links")
-    }
-    else{
-        links.classList.add("show-links")
-    }
+  navLinks.classList.toggle("visible")
 })
