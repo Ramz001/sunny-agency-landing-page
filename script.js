037410aa030bfca41d0e08a973266723d47168e6
Ramz001 <rkenjaev1@gmail.com>
2022-01-06 19:19:39 +0500

    main

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+// assigning values
 const threeBars = document.querySelector(".three-bars")
 const navLinks = document.querySelector('.nav-links')
-
-threeBars.addEventListener("click", () => {
-  navLinks.classList.toggle("visible")
-})
+// adding class to make a navbar visible 
+threeBars.addEventListener("click", () => navLinks.classList.toggle("visible"))
